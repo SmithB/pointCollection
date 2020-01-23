fc4ae817e93ecd85a958ba979ee48218b224b327
Ben Smith <bsmith@apl.washington.edu>
2020-01-23 10:43:38 -0700

    ATL06/__init__.py ATL06/tile.py: added 'tile' class to ATL06

--- ATL06/tile.py
+++ ATL06/tile.py
@@ -11,7 +11,7 @@ import pointCollection as pc
 
 class tile(pc.tile):
 
-    def __default_field_dict__():
+    def __default_field_dict__(self):
         return {None:['delta_time','h_li','h_li_sigma','latitude','longitude','atl06_quality_summary','segment_id','sigma_geo_h'],
             'fit_statistics':['dh_fit_dx'],
             'ground_track':['x_atc', 'sigma_geo_xt','sigma_geo_at'],
@@ -19,8 +19,8 @@ class tile(pc.tile):
             'orbit_info':['rgt','cycle_number'],
             'derived':['valid','matlab_time', 'n_pixels','LR','BP','spot','rss_along_track_dh']}
 
-    def __time_field__():
+    def __time_field__(self):
         return 'delta_time'
 
-    def __z_field__():
+    def __z_field__(self):
         return 'h_li'
