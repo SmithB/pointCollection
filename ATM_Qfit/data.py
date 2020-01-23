76bb1d3d3c512ce4db08cb196456e94201bf3a34
Ben Smith <bsmith@apl.washington.edu>
2020-01-23 10:46:05 -0700

    ATM_Qfit: added a reader for Qfit products

---
+++ ATM_Qfit/data.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 10:46:21 2017
+
+Class to read and manipulate ATL06 data.  Currently set up for Ben-style fake data, should be modified to work with the official ATL06 prodct foramt
+
+@author: ben
+"""
+import numpy as np
+from datetime import datetime, timedelta
+import pointCollection as pc
+import re
+
+class data(pc.data):
+    np.seterr(invalid='ignore')
+
+    def __default_field_dict__(self):
+        return {None:['latitude','longitude','elevation'], \
+                'instrument_parameters':['azimuth','rel_time'],\
+                    '__calc_internal__':['days_J2k']}
+
+    def __internal_field_calc__(self, field_dict):
+        # find the date and time number in filename
+        if 'days_J2k' in field_dict['__calc_internal__']:
+            m=re.search(r"ATM1B.*_(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d).*.h5",self.filename)
+            this_time=[int(m.group(ind+1)) for ind in range(6)]
+
+            t0=datetime(*this_time[0:3]) + timedelta(hours=this_time[3], minutes=this_time[4], seconds=this_time[5])-datetime(2000, 1, 1, 0, 0, 0)
+            t0=t0.days+t0.seconds/24./3600
+
+            self.days_J2K=t0+self.rel_time/24./3600.
