8f39ba5a7ae7763237871c0c2d0365aa52dd13aa
Ben Smith <bsmith@apl.washington.edu>
2020-01-23 10:50:00 -0700

    tile.py: added code to handle generic 'tile' methods

--- tile.py
+++ tile.py
@@ -17,33 +17,32 @@ class tile(object):
     dataset that points to each source file
     '''
 
-    def __init__(self, bin_W=[1.e4, 1.e4], tile_W=1.e5, SRS_proj4=None, time_field=None, z_field=None):
+    def __init__(self, xy0=None, bin_W=[1.e4, 1.e4], tile_W=1.e5, SRS_proj4=None, time_field=None, z_field=None):
         self.bin_W=bin_W
         self.tile_W=tile_W
         self.SRS_proj4=SRS_proj4
         self.D=None
-        self.xy0=None
+        self.xy0=xy0
         if time_field is None:
             self.time_field = self.__time_field__()
         if z_field is None:
             self.z_field = self.__z_field__()
-        return self
 
     def __default_field_dict__(self):
         return {'None':['x','y','z','time']}
 
-    def __time_field__():
+    def __time_field__(self):
         return 'time'
 
-    def __z_field__():
+    def __z_field__(self):
         return 'z'
 
-    def from_geoIndex(self, xy0,  GI_file=None, field_dict=None, out_dir=None):
+    def from_geoIndex(self, GI_file=None, field_dict=None):
 
         if field_dict is None:
-            fields=self.__default_field_dict__()
-        dxb, dyb = np.meshgrid(np.arange(-self.tile_W/2, self.tile_W/2+self.bin_W, self.bin_W),
-                           np.arange(-self.tile_W/2, self.tile_W/2+self.bin_W, self.bin_W))
+            field_dict=self.__default_field_dict__()
+        dxb, dyb = np.meshgrid(np.arange(-self.tile_W/2, self.tile_W/2+self.bin_W[0], self.bin_W[0]),
+                           np.arange(-self.tile_W/2, self.tile_W/2+self.bin_W[1], self.bin_W[1]))
         dxb=dxb.ravel()
         dyb=dyb.ravel()
 
@@ -53,12 +52,17 @@ class tile(object):
                 fields.append(ds)
 
         gI=pc.geoIndex().from_file(GI_file, read_file=False)
-        self.D=gI.query_xy((xy0[0]+dxb, xy0[1]+dyb), fields=fields)
+        self.D=gI.query_xy((self.xy0[0]+dxb, self.xy0[1]+dyb), fields=field_dict)
         return self
 
-    def write(self, out_dir, xy0, fields):
+    def write(self, out_dir, fields=None):
         if self.D is None:
             return
+        if fields is None:
+            field_dict=self.__default_field_dict__()
+            fields=[]
+            for key in field_dict.keys():
+                fields += field_dict[key]
         file_dict={}
         for file_num, Di in enumerate(self.D):
             if not hasattr(Di,'x'):
@@ -67,13 +71,13 @@ class tile(object):
 
             Di.ravel_fields()
             Di.index(np.isfinite(getattr(Di, self.z_field)))
-        file_dict[file_num]=Di.filename
+            file_dict[file_num]=Di.filename
         out_fields=list(set(fields+['x','y','source_file_num']))
         D_all=pc.data(fields=out_fields).from_list(self.D)
 
-        out_file=out_dir+'/E%d_N%d' %(xy0[0], xy0[1])
+        out_file=out_dir+'/E%d_N%d.h5' %(self.xy0[0], self.xy0[1])
 
-        pc.indexedH5(filename=out_file, delta=self.tile_W).data_to_file(D_all, out_file, time_field=self.time_field)
+        pc.indexedH5.data(filename=out_file, bin_W=self.bin_W).to_file(D_all, out_file, time_field=self.time_field)
         with h5py.File(out_file,'r+') as h5f:
             grp=h5f.create_group("source_files")
             for key in file_dict:
