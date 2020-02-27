d2cbdd2e3744f31084f780c98df0f97924d802e0
Ben Smith <bsmith@apl.washington.edu>
2020-02-27 12:57:14 -0700

    pt_blockmedian.py: made the inner loop faster by removing the index-generation step.

--- pt_blockmedian.py
+++ pt_blockmedian.py
@@ -6,7 +6,10 @@ Created on Thu Sep  6 15:44:31 2018
 """
 
 import numpy as np
-def pt_blockmedian(xx, yy, zz, delta, xy0=[0.,0.], return_index=False):
+def pt_blockmedian(xx, yy, zz, delta, xy0=[0.,0.], return_index=False, index_only=False):
+
+    if index_only:
+        return_index=True
     temp=np.isfinite(zz)
     x=xx[temp]
     y=yy[temp]
@@ -27,37 +30,43 @@ def pt_blockmedian(xx, yy, zz, delta, xy0=[0.,0.], return_index=False):
     zs=z[ind]
     xyind=xyind[ind]
     ux, ix=np.unique(xyind, return_index=True)
-    xm=np.zeros_like(ux)+np.NaN
-    ym=np.zeros_like(ux)+np.NaN
-    zm=np.zeros_like(ux)+np.NaN
+    if not index_only:
+        xm=np.zeros_like(ux)+np.NaN
+        ym=np.zeros_like(ux)+np.NaN
+        zm=np.zeros_like(ux)+np.NaN
     if return_index:
         ind=np.zeros((ux.size,2), dtype=int)
     
     ix=np.hstack((ix.ravel(), xyind.size))
     for  count, i0 in enumerate(ix[:-1]):
-        ii=np.arange(i0, ix[count+1], dtype=int)
-        iM=np.maximum(ii.size/2.-1, 0)
-        if (iM-np.floor(iM)==0) and ii.size>1:
+        # number of elements in bin
+        ni=ix[count+1]-i0
+        # index of median in the bin
+        iM=ni/2.-1
+        if (iM-np.floor(iM)==0) and ni>1:
             iM=int(np.floor(iM))
-            iM=ii[[iM, iM+1]]
-            xm[count]=(xs[iM[0]]+xs[iM[1]])/2.
-            ym[count]=(ys[iM[0]]+ys[iM[1]])/2.
-            zm[count]=(zs[iM[0]]+zs[iM[1]])/2.
+            iM=np.array([i0+iM, i0+iM+1])
+            if not index_only:
+                xm[count]=(xs[iM[0]]+xs[iM[1]])/2.
+                ym[count]=(ys[iM[0]]+ys[iM[1]])/2.
+                zm[count]=(zs[iM[0]]+zs[iM[1]])/2.
             if return_index:
                 ind[count,:]=iM
         else:
-            try:
-                iM=ii[int(iM)]
-            except IndexError:
-                print("HERE")
-            xm[count]=xs[iM]
-            ym[count]=ys[iM]
-            zm[count]=zs[iM]
+            # odd case: iM ends in 0.5.  if ni=3, iM=3/2-1 = 0.5, want 1
+            # if ni=1, iM=-0.5, want 0
+            iM=int(i0+np.floor(iM)+1)
+            if not index_only:
+                xm[count]=xs[iM]
+                ym[count]=ys[iM]
+                zm[count]=zs[iM]
             if return_index:
                 ind[count,:]=iM
         #plt.figure(1); plt.clf(); plt.subplot(211); plt.cla(); plt.scatter(xs[ii]-xm[count], ys[ii]-ym[count], c=zs[ii]); plt.axis('equal'); plt.subplot(212); plt.plot(zs[ii]); 
         #plt.pause(1)
         #print(count)
+    if index_only:
+        return ind
     if return_index:
         return xm, ym, zm, ind
     else:
