e2134d8b1b3e2a1064965687aebc519c152fd488
Ben Smith <bsmith@apl.washington.edu>
2022-02-02 11:40:52 -0800

    __init__.py: minor organizational changes

--- pointCollection/__init__.py
+++ pointCollection/__init__.py
@@ -5,9 +5,11 @@ Created on Wed Oct 16 13:36:21 2019
 
 @author: ben
 """
-
+# base classes
 from .data import data
 from .tile import tile
+from .geoIndex import geoIndex
+# data classes
 from pointCollection import ATM_Qfit
 from pointCollection import CS2_wfm
 from pointCollection import CS2
@@ -20,14 +22,14 @@ from pointCollection import glah12
 from pointCollection import ATL11
 from pointCollection import ATL11_prerelease
 from pointCollection import grid
+# utilities
 from .tools.bin_rows import bin_rows
-#from .pt_blockmedian import pt_blockmedian
 from .tools.pt_blockmedian import pt_blockmedian
-from .geoIndex import geoIndex
+from .tools.unique_by_rows import unique_by_rows
+from .tools.along_track_coords import along_track_coords
 from .points_to_grid import points_to_grid
 from .points_to_grid import apply_bin_fn
 from .xover_search import cross_tracks
 from pointCollection.ps_scale_for_lat import ps_scale_for_lat
-from .tools.unique_by_rows import unique_by_rows
 from pointCollection.reconstruct_ATL06_tracks import reconstruct_ATL06_tracks
 from pointCollection.dataPicker import dataPicker
