5738f7dbdd24e6069243f644c9806adad8dcdf33
Ben Smith <bsmith@apl.washington.edu>
2022-03-28 20:53:24 -0700

    scripts/uproot_geoindex.py : improved error handling

--- scripts/uproot_geoindex.py
+++ scripts/uproot_geoindex.py
@@ -5,6 +5,9 @@ import sys
 
 
 with h5py.File(sys.argv[1],'r+') as h5f:
-    print(f"old dir_root attribute was {h5f['index'].attrs['dir_root']}")
-    h5f['index'].attrs['dir_root']=''
-
+    print(sys.argv[1])
+    if 'dir_root' in h5f['index'].attrs:
+        print(f"\told dir_root attribute was {h5f['index'].attrs['dir_root']}")
+        h5f['index'].attrs['dir_root']=''
+    else:
+        print("\tindex has no dir_root attribute")
