be3e2b2f255900c90d0f5a75a70fc2a610726761
Ben Smith <bsmith@apl.washington.edu>
2023-01-13 17:52:53 +0000

    scripts/cross_ATL11_tile.py: add option to read crossovers based on a glob rather than a geoindex

--- scripts/cross_ATL11_tile.py
+++ scripts/cross_ATL11_tile.py
@@ -15,16 +15,26 @@ import glob
 #import re
 #import sys
 
-
-def ATL11_crossovers(index_file, xy0, W, EPSG=3031):
-    field_dict_11={None:['latitude','longitude','delta_time',\
-                        'h_corr','h_corr_sigma','h_corr_sigma_systematic', 'ref_pt'],\
-                        '__calc_internal__' : ['rgt'],
-                        'cycle_stats' : {'tide_ocean','dac'},
-                        'ref_surf':['e_slope','n_slope', 'x_atc', 'fit_quality', 'dem_h']}
+def read_data_from_index(index_file, xy0, W, field_dict_11=None):
+    
     D = pc.geoIndex().from_file(index_file).query_xy_box(
                 xy0[0]+np.array([-1, 1])*W/2, xy0[1]+np.array([-1, 1])*W/2, \
                 field_dict=field_dict_11)
+    return D
+
+def read_data_from_glob(glob_str, field_dict_11=None):
+    D=[]
+    for file in glob.glob(glob_str):
+        for pair in [1, 2, 3]:
+            try:
+                
+                D += [pc.ATL11.data(pair=pair).from_h5(file, field_dict = field_dict_11)]
+            except Exception as e:
+                print(e)
+                print(f"no data for pair {pair}, file {file}")
+    return D
+
+def ATL11_crossovers(D, EPSG=3031):
     for Di in D:
         Di.get_xy(EPSG=EPSG)
         Di.assign({'pair':np.zeros_like(Di.rgt)+Di.pair})
@@ -40,7 +50,10 @@ def ATL11_crossovers(index_file, xy0, W, EPSG=3031):
             if xyC is not None:
                 xover_list.append({'xyC':xyC, 'data_0':D[ii][inds[0]], 'data_1':D[jj][inds[1]], 'L0':L[0], 'L1':L[1]})
     N_cols=xover_list[0]['data_0'].x.shape[1]
-    v_fields=['latitude', 'longitude', 'h_corr', 'h_corr_sigma', 'h_corr_sigma_systematic', 'delta_time', 'quality_summary', 'ref_pt', 'rgt', 'pair', 'dem_h', 'x_atc', 'fit_quality', 'cycle_number', 'x', 'y']
+    v_fields=['latitude', 'longitude', 'h_corr', 'h_corr_sigma', 'h_corr_sigma_systematic', 'delta_time', 'ref_pt', 'rgt', 'pair', 'dem_h', 'x_atc', 'fit_quality', 'cycle_number', 'x', 'y']
+
+    if 'dh_geoloc' in xover_list[0]['data_0'].fields:
+        v_fields += ['dh_geoloc']
 
     v_list=[{field:np.zeros([len(xover_list), N_cols])+np.NaN for field in v_fields} for ii in [0, 1]]
     for count, X in enumerate(xover_list):
@@ -50,16 +63,13 @@ def ATL11_crossovers(index_file, xy0, W, EPSG=3031):
                 vi[field][count,:]=w.dot(getattr(data, field))
     v=[pc.data(columns=N_cols).from_dict(vi) for vi in v_list]
     
-    ind=(np.abs(v[0].x -xy0[0])<=W/2) & (np.abs(v[0].y -xy0[1])<=W/2) 
-    for ii in [0,1]:
-        v[ii].index(ind)
-    
     return v
 
-def write_xovers(v, out_dir, xy0):
-    if not os.path.isdir(out_dir):
+def write_xovers(v, out_dir, xy0, out_file):
+    if out_dir is not None and not os.path.isdir(out_dir):
         os.mkdir(out_dir)
-    out_file=os.path.join(out_dir, f'E{int(np.round(xy0[0]/1000))}_N{int(np.round(xy0[0]/1000))}.h5')
+    if out_file is None:
+        out_file=os.path.join(out_dir, f'E{int(np.round(xy0[0]/1000))}_N{int(np.round(xy0[0]/1000))}.h5')
     v[0].to_h5(out_file, group='data_0', replace=True)
     v[1].to_h5(out_file, group='data_1', replace=False)
     
@@ -81,29 +91,40 @@ def main():
     parser.add_argument('--xy0', type=float, nargs=2, help="fit center location")
     parser.add_argument('--Width','-W',  type=float, help="fit width")
     parser.add_argument('--out_dir', type=str, help="output directory")
+    parser.add_argument('--glob_str','-g', type=str, help="glob string to find files to cross, to be specified if xy0 and W are not")
+    parser.add_argument('--out_file', type=str, help="output file, to be specified if xy0 is not")
     parser.add_argument('--hemisphere', '-H', type=int, help="hemisphere, -1 for Antarctica, 1, for Arctic")
     parser.add_argument('--queue','-q', action="store_true")
     args=parser.parse_args()
     
-    hemisphere=args.hemisphere
-    out_dir=args.out_dir
     
-    if hemisphere==1:
+    if args.hemisphere==1:
         EPSG=3413
     else:
         EPSG=3031
     
-    if not os.path.isdir(out_dir):
-        os.mkdir(out_dir)
+    if args.out_dir is not None and not os.path.isdir(args.out_dir):
+        os.mkdir(args.out_dir)
 
     if args.queue:
-        make_queue(args.index_file, args.W, hemisphere, out_dir)
+        make_queue(args.index_file, args.W, args.hemisphere, args.out_dir)
         return
 
-    v=ATL11_crossovers(args.index_file, args.xy0, args.W, EPSG=EPSG)
-    if len(xover_list) > 0:
-        write_xovers(v, args.out_dir, args.xy0)
+    field_dict_11={None:['latitude','longitude','delta_time',\
+                        'h_corr','h_corr_sigma','h_corr_sigma_systematic', 'ref_pt'],\
+                        '__calc_internal__' : ['rgt'],
+                        'cycle_stats' : {'tide_ocean','dac','dh_geoloc'},
+                        'ref_surf':['e_slope','n_slope', 'x_atc', 'fit_quality', 'dem_h']}
 
+    if args.glob_str is not None:
+        D=read_data_from_glob(args.glob_str, field_dict_11=field_dict_11)
+    else:
+        D=read_data_from_index(args.index_file, args.xy0, args.W, field_dict_11=field_dict_11)
+        
+    v=ATL11_crossovers(D, EPSG=EPSG)
+    
+    if len(v) > 0:
+        write_xovers(v, args.out_dir, args.xy0, args.out_file)
 
 if __name__=='__main__':
     main()
