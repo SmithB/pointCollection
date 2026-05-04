9d489993c16b6a633e113c6d206cfc29d7cc7016
Ben Smith <bsmith@apl.washington.edu>
2026-05-04 09:23:47 -0700

    pointCollection/scripts/index_glob.py: by default, add new files to existing geoIndex

--- pointCollection/scripts/index_glob.py
+++ pointCollection/scripts/index_glob.py
@@ -11,16 +11,21 @@ import pointCollection as pc
 import os
 import glob
 import numpy as np
-import matplotlib.pyplot as plt
+import re
 
 import argparse
 
 
-def index_for_glob(glob_string, dir_root=None, index_file=None, file_type=None, group=None,\
+def index_for_glob(glob_string,
+                   dir_root=None,
+                   index_file=None,
+                   replace=False,
+                   file_type=None,
+                   group=None,
                    verbose=False, delta=[1.e4, 1.e4], SRS_proj4=None, relative=False):
     """
     make a geoindex for the files in a glob string
-    
+
     arguments:
         glob_string: a string that when passed to glob will return a list of files
         dir_root: directory name in which to root the index
@@ -28,15 +33,34 @@ def index_for_glob(glob_string, dir_root=None, index_file=None, file_type=None, 
         verbose: should a lot be echoed?  defaults to None
     """
 
+    if index_file is None:
+        index_file=os.path.join(os.path.dirname(glob_string),'GeoIndex.h5')
+
     if dir_root is not None and dir_root[-1] != '/':
         dir_root += '/'
 
+    if os.path.isfile(index_file) and not replace:
+        gI = pc.geoIndex().from_file(index_file, read_file=True)
+        file_re = re.compile(r'file_\d+')
+        have_files=[]
+        for key, file in gI.attrs.items():
+            if file_re.search(key) is None:
+                continue
+            have_files.append( os.path.abspath(gI.resolve_path(file)) )
+    else:
+        gI=pc.geoIndex(delta=delta, SRS_proj4=SRS_proj4)
+        have_files = []
+    if verbose:
+        print(f'index_glob : have {len(have_files)} files')
     files=[]
-    for thestr in glob_string.split(' '):
-        files += glob.glob(thestr)
 
-    if index_file is None:
-        index_file=os.path.join(os.path.dirname(glob_string),'GeoIndex.h5')
+    for thestr in glob_string.split(' '):
+        for thefile in glob.glob(thestr):
+            if os.path.abspath(thefile) not in have_files:
+                files.append(thefile)
+
+    if verbose:
+        print(f'index_glob : adding {len(files)} files')
 
     index_list=[];
     for file in files:
@@ -56,41 +80,44 @@ def index_for_glob(glob_string, dir_root=None, index_file=None, file_type=None, 
     if relative:
         gI.attrs['dir_root']=None
     gI.to_file(index_file)
-    
+
 def main():
     parser=argparse.ArgumentParser()
     parser.add_argument('--glob_string','-g', type=str, required=True, help="quoted string to pass to glob to find the files to index")
-    parser.add_argument('--type','-t', type=str, required=True, help="file type.  See pointCollection.geoIndex for options")    
+    parser.add_argument('--type','-t', type=str, required=True, help="file type.  See pointCollection.geoIndex for options")
     parser.add_argument('--group', type=str, required=False, help="group within the file to read.  Defaults to None")
     parser.add_argument('--index_file','-i', type=str, help="index file, defaults to GeoIndex.h5 in the dirname of the glob string")
     parser.add_argument('--dir_root','-r', type=str, help="directory root for the index.  Defaults to None (absolute paths)")
     parser.add_argument('--bin_size', '-b', type=int, default=1.e4,  help='index bin size, default=1.e4')
     parser.add_argument('--hemisphere','-H', type=int, help='hemisphere, must be 1 or -1,')
     parser.add_argument('--proj4', type=str, help='proj4 to be used for the tiles')
+    parser.add_argument('--replace', action='store_true', help='replace index file if it exists')
     parser.add_argument('--verbose','-v', action='store_true')
     parser.add_argument('--Relative','-R', action='store_true')
     args=parser.parse_args()
-    
-    if args.hemisphere==1:
+
+    if args.hemisphere == 1:
         srs_proj4 = '+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs '
-    elif args.hemisphere==-1:
+    elif args.hemisphere == -1:
         srs_proj4 = '+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs '
     if args.proj4 is not None:
         srs_proj4 = args.proj4
-    
-    index_for_glob(args.glob_string, dir_root=args.dir_root, \
-                   index_file=args.index_file, \
-                   file_type=args.type,\
-                   verbose=args.verbose,\
-                   delta=[np.float64(args.bin_size), np.float64(args.bin_size)], \
-                   SRS_proj4=srs_proj4,\
-                   group=args.group,\
-                   relative=args.Relative)
+
+    index_for_glob(args.glob_string,
+                   dir_root=args.dir_root,
+                   index_file=args.index_file,
+                   file_type=args.type,
+                   verbose=args.verbose,
+                   delta=[np.float64(args.bin_size), np.float64(args.bin_size)],
+                   SRS_proj4=srs_proj4,
+                   group=args.group,
+                   relative=args.Relative,
+                   replace=args.replace)
 
 if __name__=='__main__':
     main()
 
 # -H 1 -g "/Volumes/ice2/ben/scf/GL_11/U01/*.h5" --index_file /Volumes/ice2/ben/scf/GL_11/U01/GeoIndex.h5 --dir_root /Volumes/ice2/ben/scf/GL_11/U01 -t "h5_geoindex"
-    
-    
-   #-H 1 -g "/Volumes/ice2/ben/scf/GL_11/U01*/ATL11*.h5" --index_file /Volumes/ice2/ben/scf/GL_11/GeoIndex_U01.h5 --dir_root /Volumes/ice2/ben/scf/GL_11/ -t "h5_geoindex" 
+
+
+   #-H 1 -g "/Volumes/ice2/ben/scf/GL_11/U01*/ATL11*.h5" --index_file /Volumes/ice2/ben/scf/GL_11/GeoIndex_U01.h5 --dir_root /Volumes/ice2/ben/scf/GL_11/ -t "h5_geoindex"
